27a6914aadd17b79465d0a52ea68cb8e3198c094
HaoXiangL <657809680@qq.com>
2019-04-08 16:02:05 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -8,9 +8,13 @@ import 'style/reset.css'
 import 'style/border.css'
 // iconfont库引入
 import 'style/iconfont.css'
+// vueawesomeswiper库引入(轮播)
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <Header></Header>
+    <swiper></swiper>
+    <div>test</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '../components/header/Header'
+import Swiper from '../components/Swiper/Swiper'
 
 export default {
   name: 'home',
   components: {
-    Header
+    Header,
+    Swiper
   }
 }
 </script>
