60159526a07fda233d596e3c5f10e9d2851dc909
HaoXiangL <657809680@qq.com>
2019-04-09 17:56:20 +0800

    ajax city

--- src/views/City.vue
+++ src/views/City.vue
@@ -2,18 +2,48 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list :cities="cities" :hot="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from '../components/CityHeader'
 import CitySearch from '../components/CitySearch'
+import CityList from '../components/CityList'
+import CityAlphabet from '../components/CityAlphabet'
 
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCitySucc)
+    },
+    handleGetCitySucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
